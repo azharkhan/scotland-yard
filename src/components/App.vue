02eb930eeecb9ba74f6247d25ce826891e661b1b
Azhar Khan <akhan@hubba.com>
2020-06-07 01:26:20 -0400

    adds Vue app & components
    - simplifies index and adds Vue app
    - breaks up game into components

---
+++ src/components/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="game">
+    <div class="game-board">
+      <Map />
+    </div>
+    <div class="players">
+      <div class="player mr-x">
+        <div class="info">
+          <h1>{{ name }}</h1>
+        </div>
+      </div>
+      <Detective
+        v-for="detective in detectives"
+        :key="detective"
+        :num="detective"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Map from "./Map.vue";
+import Detective from "./Detective.vue";
+
+export default {
+  name: "App",
+  components: { Map, Detective },
+  data() {
+    return {
+      name: "Mr. X",
+      detectives: [1, 2, 3, 4, 5],
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Detective.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="player" :class="`detective-${num}`">
+    <div class="info">
+      <h1>Detective {{ num }}</h1>
+      <h2 class="location"></h2>
+      <ul class="transportation">
+        <li>Taxi:</li>
+        <li>Bus:</li>
+        <li>Underground:</li>
+      </ul>
+      <button class="set-location">Set Location</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Detective",
+  props: {
+    num: Number,
+  },
+};
+</script>
