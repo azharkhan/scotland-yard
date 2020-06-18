c279f48a49cb3bc0220251d212c2a4e648740c1d
Azhar Khan <akhan@hubba.com>
2020-06-18 00:57:38 -0400

    adds vue-router
    why?
    - we need some routing in order to create a new game or join an existing
      game.
    
    TODO: modify the current app to dynamically get a game from a given id
    
    - adds vue router
    - creates a skeleton landing page
    - adds some pages as placeholders

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="hero">
+      <h1>Welcome Muscateers!</h1>
+      <div class="options">
+        <button>Start Game</button>
+        <button>Join Game</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "LandingPage",
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import { firestorePlugin } from "vuefire";
 import App from "./App.vue";
+import router from './router'
 
 Vue.config.productionTip = false;
 Vue.use(firestorePlugin);
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount("#app");
