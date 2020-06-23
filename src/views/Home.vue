8ae53c23492e416b5bec5520c03d36c2e9e98721
Azhar Khan <akhan@hubba.com>
2020-06-22 21:18:23 -0400

    fixes game creation
    - adds initial objects for all items
    - sets initial tickets
    - sets locations for all players

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,6 +17,10 @@
 import { db } from "@/db.js";
 import randomWords from "random-words";
 
+const allLocations = Array(200)
+  .fill(1)
+  .map((i, index) => index + 1);
+
 export default {
   name: "Home",
   data() {
@@ -25,37 +29,100 @@ export default {
         round: 1,
         playerOnTurn: "mr-x",
         isInProgress: true,
-      },
-      detectiveProps: {
-        role: "detective-1",
-        tickets: {
-          taxi: 11,
-          bus: 8,
-          underground: 4,
+        result: "",
+        "mr-x": {
+          role: "mr-x",
         },
+      },
+      allLocations,
+      totalTickets: {
+        taxi: 57,
+        bus: 45,
+        underground: 23,
       },
     };
   },
   computed: {
-    newGameUrl() {
+    newGameId() {
       return randomWords(3).join("-");
+    },
+    shuffledPositions() {
+      const allLocationsCopy = this.allLocations.slice();
+
+      // shuffle an array
+      // ref: https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
+      for (let i = allLocationsCopy.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * i);
+        const temp = allLocationsCopy[i];
+        allLocationsCopy[i] = allLocationsCopy[j];
+        allLocationsCopy[j] = temp;
+      }
+      return allLocationsCopy;
     },
   },
   methods: {
-    createNewGame: function() {
-      db.collection("games")
-        .doc(this.newGameUrl)
-        .set({ ...this.gameProps })
-        .then(() => {
-          db.collection("games")
-            .doc(this.newGameUrl)
+    getRandomPosition: function() {
+      this.allLocations = this.shuffledPositions;
+      return this.allLocations.pop();
+    },
+    getDetectiveTickets: function() {
+      const tickets = {
+        taxi: 11,
+        bus: 8,
+        underground: 4,
+      };
+
+      for (let type in tickets) {
+        // adjust total available tickets
+        this.totalTickets[type] = this.totalTickets[type] - tickets[type];
+      }
+
+      return tickets;
+    },
+    createNewGame: async function() {
+      await db
+        .collection("games")
+        .doc(this.newGameId)
+        .set({ ...this.gameProps });
+      await this.createDetectiveCollection();
+      await this.addMrX();
+      this.$router.push({ name: "Game", params: { id: this.newGameId } });
+    },
+    addMrX: async function() {
+      const randomPosition = this.getRandomPosition();
+
+      const mrX = {
+        role: "mr-x",
+        tickets: this.totalTickets,
+        currentLocation: randomPosition,
+        moves: [{ position: randomPosition }],
+      };
+
+      return await db
+        .collection("games")
+        .doc(this.newGameId)
+        .update({ "mr-x": mrX });
+    },
+    createDetectiveCollection: async function() {
+      const detectivePromises = Array(5)
+        .fill(1)
+        .map((num, index) => {
+          let position = this.getRandomPosition();
+          let tickets = this.getDetectiveTickets();
+
+          return db
+            .collection("games")
+            .doc(this.newGameId)
             .collection("detectives")
-            .doc("detective-1")
-            .set({ ...this.detectiveProps });
-        })
-        .then(() => {
-          this.$router.push({ name: "Game", params: { id: this.newGameUrl } });
+            .doc()
+            .set({
+              role: `detective-${index + 1}`,
+              currentLocation: position,
+              tickets,
+            });
         });
+
+      return await Promise.all(detectivePromises);
     },
   },
 };
