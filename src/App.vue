715286ad288e888e8040881af9cdb65985066fbc
Azhar Khan <akhan@hubba.com>
2020-06-29 16:27:17 -0400

    removed deprecated game link

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,6 @@
     <b-navbar>
       <template slot="start">
         <b-navbar-item tag="router-link" :to="{ path: '/' }">Home</b-navbar-item>
-        <b-navbar-item
-          tag="router-link"
-          :to="{ name: 'Game', params: { id: 'tcZwNAgeeZuJBzNl48l1' } }"
-        >Current Game</b-navbar-item>
       </template>
       <template slot="end">
         <b-navbar-item tag="div" v-if="!state.user">
